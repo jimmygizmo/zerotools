c974b072db303968a8e5846ed3a2bd518c96577b
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2018-08-01 18:24:14 -0700

    can now run the app jar by specifying jar as classpath but not just the jar, so the next thing to fix is to correctly make this an executable jar.

--- springhello/src/main/java/com/ninthdevice/helloworld/HelloWorldApplication.java
+++ springhello/src/main/java/com/ninthdevice/helloworld/HelloWorldApplication.java
@@ -6,6 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Test in browser at http://localhost:8080/hello?name=Wooorrrld
+ *
+ * Current Maven build config works to run this standalone specifying classpath like this:
+ * java -cp ./helloworld.jar com.ninthdevice.helloworld.HelloWorldApplication
+ * BUT not like this:
+ * java -jar ./helloworld.jar
+ * TODO: So our jar is not yet an executable jar.
+ *
  * */
 @SpringBootApplication
 public class HelloWorldApplication {
