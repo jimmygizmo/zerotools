a56fabb3f48cb997702fef2629351e66cf0e3fbb
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2018-11-28 13:52:14 -0800

    freshly initialized xcode project set up for a realtime google maps app using cocoapods

---
+++ Novemap/Novemap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Novemap
+//
+//  Created by Jim Mannix on 11/27/18.
+//  Copyright © 2018 Ninth Device. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
